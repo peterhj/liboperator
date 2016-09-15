2be63e9c7b68a077d553cba67c5e5f2aeac40b5b
Peter Jin <peterhaijin@gmail.com>
2016-09-15 16:58:49 -0700

    Reader/writer methods should all accept an offset, for now.

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,7 @@ pub enum Regularization {
 
 pub trait Operator<T, S>: InternalOperator<T> {
   fn load_data(&mut self, samples: &[S]);
+  fn store_loss(&mut self) -> f32 { unimplemented!(); }
 }
 
 pub trait InternalOperator<T> {
@@ -36,16 +37,18 @@ pub trait InternalOperator<T> {
   fn save_rng_state(&mut self) {}
   fn restore_rng_state(&mut self) {}
 
-  fn init_state(&mut self) {}
+  //fn init_state(&mut self) {}
 
   fn init_param(&mut self, _rng: &mut Xorshiftplus128Rng) {}
-  fn load_param(&mut self, _param_reader: &mut ReadBuffer<T>) -> usize { 0 }
-  fn store_param(&mut self, _param_writer: &mut WriteBuffer<T>) -> usize { 0 }
+  fn load_param(&mut self, _param_reader: &mut ReadBuffer<T>, _offset: usize) -> usize { 0 }
+  fn store_param(&mut self, _param_writer: &mut WriteBuffer<T>, _offset: usize) -> usize { 0 }
   fn update_param(&mut self, _alpha: f32, _beta: f32, _grad_reader: &mut ReadAccumulateBuffer<T>, _offset: usize) -> usize { 0 }
 
+  fn reset_loss(&mut self) {}
+
   fn reset_grad(&mut self) {}
-  fn load_grad(&mut self, _grad_reader: &mut ReadBuffer<T>) -> usize { 0 }
-  fn store_grad(&mut self, _grad_writer: &mut WriteBuffer<T>) -> usize { 0 }
+  fn load_grad(&mut self, _grad_reader: &mut ReadBuffer<T>, _offset: usize) -> usize { 0 }
+  fn store_grad(&mut self, _grad_writer: &mut WriteBuffer<T>, _offset: usize) -> usize { 0 }
   fn apply_grad_reg(&mut self, _reg: Regularization) {}
   fn accumulate_grad(&mut self, _alpha: f32, _beta: f32, _grad_accum: &mut AccumulateBuffer<T>, _offset: usize) -> usize { 0 }
 
